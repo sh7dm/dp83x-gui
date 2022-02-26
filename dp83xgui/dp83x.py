2afd9e6c5f16a6aefabfcee4c5a398eb126c3a62
ornea <justin.richards@gmail.com>
2022-02-26 16:36:28 +0800

    Update dp83x.py
    Original commit from me

--- dp83xgui/dp83x.py
+++ dp83xgui/dp83x.py
@@ -1,20 +1,21 @@
+# pip install pyvisa-py
+import pyvisa as visa
 
-#Install pyvisa, use easy_install for example:
-#easy_install pyvisa
-import visa
-
-class DP832(object):
+class DP83X(object):
     def __init__(self):
         pass
 
-    def conn(self, constr="USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR"):
+    def conn(self, constr="TCPIP0::192.168.1.60::INSTR"):# "USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR"):
         """Attempt to connect to instrument"""
         rm = visa.ResourceManager()
         self.inst = rm.open_resource(constr)
 
     def identify(self):
         """Return identify string which has serial number"""
-        return self.inst.query("*IDN?")
+        resp = self.inst.query("*IDN?").rstrip("\n").split(',')
+        dr = {"company":resp[0], "model":resp[1], "serial":resp[2], "ver":resp[3]}
+        
+        return dr
 
     def readings(self, channel="CH1"):
         """Read voltage/current/power from CH1/CH2/CH3"""       
@@ -25,14 +26,52 @@ class DP832(object):
 
     def dis(self):
         del self.inst
+    
+    def applyVoltage(self,channel, voltage):
+        self.inst.write(":APPL %s,%s"% (channel, voltage))
+        
+    def applyCurrent(self,channel, current):
+        print(":SOURce %s :CURRent %s"%(channel, current))
+        self.inst.write(":SOURce%s:CURRent %s"%(channel, current))
+
+    def queryVolt(self, channel):
+        return(float((self.inst.query(":APPL? %s ,VOLTage"%channel)).rstrip("\n")))
+    
+    def queryCurr(self, channel):
+        return(float((self.inst.query(":APPL? %s ,CURRent"%channel)).rstrip("\n")))
+
+    def off(self,channels=["Ch1","CH2","CH3"]):
+        for channel in channels:
+            self.inst.write("OUTP " + channel + ",OFF")
+            
+    def eStop(self):
+        self.inst.write("OUTP ALL ,OFF")
+        
+    def allOn(self):
+        self.inst.write("OUTP ALL ,ON")
+            
+    def on(self,channel="CH1"):
+        self.inst.write("OUTP " + channel + ",on")
+        print(channel)
+            
+    def state(self,channel="CH1"):
+        return((self.inst.query("OUTP? " + channel )).rstrip("\n"))
+        
+    def applyState(self,channel="CH1",state="OFF"):
+        print(channel)
+        print(state)
+        self.inst.write("OUTP %s,%s"%(channel, state))
 
     def writing(self, command=""):
         self.inst.write(command)
+        
+    def temperature(self):
+        return((self.inst.query(":SYSTem:SELF:TEST:TEMP?")).rstrip("\n"))
         
 if __name__ == '__main__':
     test = DP832()
 
     #Insert your serial number here / confirm via Ultra Sigma GUI
-    test.conn("USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR")
+    test.conn("TCPIP0::172.16.0.125::INSTR")#"USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR")
     
-    print test.readings()
+    print (test.readings())
